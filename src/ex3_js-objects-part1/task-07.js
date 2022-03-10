3edb3987ed1f4b835c42c802b5f042a8d4145b8f
Gotrunn <itsgotrunn@gmail.com>
2022-03-10 16:20:06 +0300

    task-07-2

--- src/ex3_js-objects-part1/task-07.js
+++ src/ex3_js-objects-part1/task-07.js
@@ -1,21 +1,22 @@
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable no-continue */
 /* eslint-disable guard-for-in */
-// module.exports = function objDeepClone(obj) {
-//   const finalObj = JSON.parse(JSON.stringify(obj));
 
-//   return finalObj;
-// };
-
+// Способ номер 1
 module.exports = function objDeepClone(obj) {
   const finalObj = {};
   for (const key in obj) {
     if (obj[key] instanceof Object) {
       finalObj[key] = objDeepClone(obj[key]);
-      continue;
     }
     finalObj[key] = obj[key];
   }
 
   return finalObj;
 };
+
+// Способ номер 2
+// module.exports = function objDeepClone(obj) {
+//   const finalObj = JSON.parse(JSON.stringify(obj));
+
+//   return finalObj;
+// };
