ece1748c8e093f39d3f6e26dd55484d9549c6710
Gotrunn <itsgotrunn@gmail.com>
2022-03-10 16:14:36 +0300

    task-07

--- src/ex3_js-objects-part1/task-07.js
+++ src/ex3_js-objects-part1/task-07.js
@@ -1,5 +1,21 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-continue */
+/* eslint-disable guard-for-in */
+// module.exports = function objDeepClone(obj) {
+//   const finalObj = JSON.parse(JSON.stringify(obj));
+
+//   return finalObj;
+// };
+
 module.exports = function objDeepClone(obj) {
-  const finalObj = JSON.parse(JSON.stringify(obj));
+  const finalObj = {};
+  for (const key in obj) {
+    if (obj[key] instanceof Object) {
+      finalObj[key] = objDeepClone(obj[key]);
+      continue;
+    }
+    finalObj[key] = obj[key];
+  }
 
   return finalObj;
 };
