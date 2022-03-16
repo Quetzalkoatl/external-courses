09b6ae42382b9aebd1b63dd5fc6b949a4d25ee4d
Gotrunn <itsgotrunn@gmail.com>
2022-03-16 21:18:33 +0300

    changes

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,18 +1,17 @@
 /* eslint-disable no-tabs */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable linebreak-style */
-/* eslint-disable indent */
 module.exports = function getUniqueSymbols(str) {
-	const strToArr = str.split('');
-	const finalObject = {};
-	for (let i = 0; i < strToArr.length; i += 1) {
-		if (finalObject[strToArr[i]] !== undefined) {
-			finalObject[strToArr[i]] += 1;
-		} else {
-			finalObject[strToArr[i]] = 1;
-		}
-	}
-	for (const key in finalObject) {
-		console.log(key, finalObject[key]);
-	}
+  const strToArr = str.split('');
+  const finalObject = {};
+  for (let i = 0; i < strToArr.length; i += 1) {
+    if (finalObject[strToArr[i]] !== undefined) {
+      finalObject[strToArr[i]] += 1;
+    } else {
+      finalObject[strToArr[i]] = 1;
+    }
+  }
+  for (const key in finalObject) {
+    console.log(key, finalObject[key]);
+  }
 };
