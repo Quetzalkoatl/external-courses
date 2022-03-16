bc171da0bc206459a359f12de87944fc716f5d17
Gotrunn <itsgotrunn@gmail.com>
2022-03-16 21:17:50 +0300

    changes

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,3 +1,19 @@
-module.exports = function uniqueSymbols(str) {
-  console.log(new Set(str).size);
+/* eslint-disable no-tabs */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable guard-for-in */
+module.exports = function getUniqueSymbols(str) {
+	const strToArr = str.split('');
+	const finalObject = {};
+	for (let i = 0; i < strToArr.length; i += 1) {
+		if (finalObject[strToArr[i]] !== undefined) {
+			finalObject[strToArr[i]] += 1;
+		} else {
+			finalObject[strToArr[i]] = 1;
+		}
+	}
+	for (const key in finalObject) {
+		console.log(key, finalObject[key]);
+	}
 };
