9399189da11bc2ceacc0d3e8ae025adc08255f0e
Gotrunn <itsgotrunn@gmail.com>
2022-04-17 20:08:44 +0300

    task-01

---
+++ src/ex11_js_dom/task-01.js
@@ -0,0 +1,64 @@
+const previous = document.getElementById('prev');
+const next = document.getElementById('next');
+const current = document.getElementById('currentImage');
+
+const images = [
+  ['../ex11_js_dom/assets/1.png'],
+  ['../ex11_js_dom/assets/2.png'],
+  ['../ex11_js_dom/assets/3.png'],
+  ['../ex11_js_dom/assets/4.png'],
+  ['../ex11_js_dom/assets/5.png'],
+];
+
+// Начальный слайд
+let allImagesLength = 0;
+let temp = images[allImagesLength];
+
+// Длительность анимации (в секундах)
+const animationLength = 1;
+
+// Функция задает изображение и стили для него
+function imageStyles() {
+  current.style.width = '500px';
+  current.style.height = '500px';
+  current.style.background = `url(${temp})`;
+  current.style.position = 'absolute';
+  current.style.left = '50%';
+  current.style.top = '50%';
+  current.style.transform = 'translate(-50%, -50%)';
+  current.style.transition = `all ${animationLength}s ease`;
+}
+
+imageStyles();
+
+// Функция отключает кнопки пока не закончится анимация перехода
+function disableButtons() {
+  previous.setAttribute('disabled', true);
+  next.setAttribute('disabled', true);
+  setTimeout(() => {
+    previous.removeAttribute('disabled');
+    next.removeAttribute('disabled');
+  }, animationLength * 1000);
+}
+
+// Переключение на предыдущий слайд
+previous.addEventListener('click', () => {
+  if (!allImagesLength) {
+    allImagesLength = images.length;
+  }
+  allImagesLength -= 1;
+  temp = images[allImagesLength];
+  imageStyles();
+  disableButtons();
+});
+
+// Переключение на следующий слайд
+next.addEventListener('click', () => {
+  if (allImagesLength === images.length - 1) {
+    allImagesLength = -1;
+  }
+  allImagesLength += 1;
+  temp = images[allImagesLength];
+  imageStyles();
+  disableButtons();
+});
