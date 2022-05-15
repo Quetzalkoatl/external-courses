2cd0ba08c5ec9b2559adb2d177156d5651e737c0
Gotrunn <itsgotrunn@gmail.com>
2022-05-15 19:52:00 +0300

    changes

--- src/ex14_js-oop/task-02.js
+++ src/ex14_js-oop/task-02.js
@@ -1,7 +1,17 @@
 /* eslint-disable no-restricted-syntax */
-class ElectricDevice {
+class Device {
   constructor(options) {
     this.name = options.name;
+    this.cost = options.cost;
+  }
+}
+
+const cup = new Device({ name: 'cup', cost: 50 });
+const screwdriver = new Device({ name: 'skrewdriver', cost: 100 });
+
+class ElectricDevice extends Device {
+  constructor(options) {
+    super(options);
     this.power = options.power;
     this.powerStatus = options.powerStatus;
   }
@@ -9,29 +19,33 @@ class ElectricDevice {
 
 const teapot = new ElectricDevice({
   name: 'teapot',
+  cost: 150,
   power: 500,
   powerStatus: true,
 });
 
 const lamp = new ElectricDevice({
   name: 'lamp',
+  cost: 300,
   power: 1000,
   powerStatus: false,
 });
 
 const smartphone = new ElectricDevice({
   name: 'smartphone',
+  cost: 800,
   power: 1500,
   powerStatus: true,
 });
 
 const laptop = new ElectricDevice({
   name: 'laptop',
+  cost: 1500,
   power: 2000,
   powerStatus: false,
 });
 
-const ROOM = [teapot, lamp, smartphone, laptop];
+const ROOM = [cup, screwdriver, teapot, lamp, smartphone, laptop];
 
 function searchForDevice(obj, name) {
   for (const key in obj) {
@@ -57,6 +71,6 @@ function totalPower(obj) {
   console.log(`Total power is: ${power}`);
 }
 
-searchForDevice(ROOM, 'laptop');
+searchForDevice(ROOM, 'cup');
 
 totalPower(ROOM);
