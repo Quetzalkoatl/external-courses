8929aae1dd99b03915f47b8306b550de4b5580de
Gotrunn <itsgotrunn@gmail.com>
2022-05-16 16:43:03 +0300

    changes

--- src/ex13_js-context/task-02.js
+++ src/ex13_js-context/task-02.js
@@ -3,61 +3,51 @@ function Hangman(word) {
   let errors = 6;
   let answer = '_'.repeat(currentWord.length);
   let wrongSymbols = [];
-  function guess(symbol) {
-    let count = 0;
-    for (let i = 0; i < currentWord.length; i += 1) {
-      if (symbol.toLowerCase() === currentWord[i].toLowerCase()) {
-        const test = answer.split('');
-        test[i] = symbol.toLowerCase();
-        answer = test.join('');
-        count += 1;
-      }
-    }
-    if (count === 0) {
-      errors -= 1;
-      wrongSymbols.push(symbol.toLowerCase());
-      console.log(
-        `wrong letter, errors left ${errors} | ${wrongSymbols.join(',')}`,
-      );
-      return this;
-    }
-    if (answer === currentWord) {
-      console.log(`${answer} | You won!`);
-      return this;
-    }
-    console.log(answer);
-    return this;
-  }
-  function getGuessedString() {
-    console.log(answer);
-    return this;
-  }
-  function getErrorsLeft() {
-    console.log(errors);
-    return this;
-  }
-  function getWrongSymbols() {
-    console.log(wrongSymbols);
-    return this;
-  }
-  function getStatus() {
-    console.log(`${answer} | errors left ${errors}`);
-    return this;
-  }
-  function startAgain(newWord) {
-    currentWord = newWord;
-    errors = 6;
-    answer = '_'.repeat(currentWord.length);
-    wrongSymbols = [];
-    return this;
-  }
   return {
-    guess,
-    getWrongSymbols,
-    getStatus,
-    getErrorsLeft,
-    getGuessedString,
-    startAgain,
+    guess(symbol) {
+      let count = 0;
+      for (let i = 0; i < currentWord.length; i += 1) {
+        if (symbol.toLowerCase() === currentWord[i].toLowerCase()) {
+          const test = answer.split('');
+          test[i] = symbol.toLowerCase();
+          answer = test.join('');
+          count += 1;
+        }
+      }
+      if (count === 0) {
+        errors -= 1;
+        wrongSymbols.push(symbol.toLowerCase());
+        console.log(
+          `wrong letter, errors left ${errors} | ${wrongSymbols.join(',')}`,
+        );
+      }
+      if (answer === currentWord) {
+        console.log(`${answer} | You won!`);
+      }
+      console.log(answer);
+      return answer;
+    },
+    getGuessedString() {
+      console.log(answer);
+      return answer;
+    },
+    getErrorsLeft() {
+      console.log(errors);
+      return errors;
+    },
+    getWrongSymbols() {
+      console.log(wrongSymbols);
+      return wrongSymbols;
+    },
+    getStatus() {
+      console.log(`${answer} | errors left ${errors}`);
+    },
+    startAgain(newWord) {
+      currentWord = newWord;
+      errors = 6;
+      answer = '_'.repeat(currentWord.length);
+      wrongSymbols = [];
+    },
   };
 }
 
